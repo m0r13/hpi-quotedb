fbadc5fd48367fba0afc615a6a36af934552ba48
m0r13 <moritz.hilscher@gmail.com>
2015-10-27 13:40:11 +0100

    "Hello World!" with Bootstrap.

--- hpi_quotedb/urls.py
+++ hpi_quotedb/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . quotedb import urls as quotedb_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^', include(quotedb_urls)),
 ]
